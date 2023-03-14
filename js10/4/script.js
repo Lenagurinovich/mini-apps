4f0d11c45618b34c782ab99aa2b6c1515164c3d3
Lenagurinovich <79358325+Lenagurinovich@users.noreply.github.com>
2023-03-14 20:51:04 +0300

    Add files via upload

--- js10/4/script.js
+++ js10/4/script.js
@@ -1,6 +1,39 @@
 const form = document.querySelector('form');
 const ul = document.querySelector('ul');
 const input = document.querySelector('input');
+const done = document.querySelector('.done');
+
+
+let tasks = [];
+if (localStorage.tasks){
+    tasks = JSON.parse(localStorage.tasks);
+}
+
+for (let task of tasks){
+    let li = document.createElement('li');
+    let checkbox = document.createElement('input');
+    let button = document.createElement('button');
+    button.innerText = 'X';
+    checkbox.type = 'checkbox';
+    checkbox.className = 'checkbox';
+    li.innerText = task;
+    li.prepend(checkbox);
+    ul.appendChild(li);
+    li.appendChild(button);
+    checkbox.addEventListener('click', (event) => {
+        if (checkbox.checked){
+            li.style.textDecoration = 'line-through';
+            ul.appendChild(li);
+        } else{
+            li.style.textDecoration = 'none';
+            ul.prepend(li);
+        }
+    })
+    button.addEventListener('click', (event) => {
+        li.remove()
+    })
+}
+
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     let li = document.createElement('li');
@@ -13,6 +46,8 @@ form.addEventListener('submit', (event) => {
     li.prepend(checkbox);
     ul.appendChild(li);
     li.appendChild(button);
+    tasks.push(input.value);
+    localStorage.tasks = JSON.stringify(tasks);
     input.value = '';
     checkbox.addEventListener('click', (event) => {
         if (checkbox.checked){
@@ -27,3 +62,9 @@ form.addEventListener('submit', (event) => {
         li.remove()
     })
 })
+done.addEventListener('click', (event) => {
+    document.querySelectorAll('li').forEach(item => {
+        item.style.textDecoration = 'line-through';
+    })
+})
+
