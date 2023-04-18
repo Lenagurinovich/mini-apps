6ef22ee3ea2a55fd8f4f3d4a31f91f6205b3d909
Lenagurinovich <79358325+Lenagurinovich@users.noreply.github.com>
2023-04-18 19:25:23 +0300

    Add files via upload

--- js12/2/script.js
+++ js12/2/script.js
@@ -4,20 +4,11 @@ const btn = document.querySelector('button')
 const body = document.querySelector('body');
 
 btn.addEventListener('click', () => {
+    body.classList.toggle('dark');
     if (btn.classList.contains('light')){
         btn.innerText = 'light theme';
-        nav.classList.add('dark');
-        btn.classList.add('dark');
-        btn.classList.remove('light');
-        body.classList.add('dark');
-        console.log(1);
     } else if (btn.classList.contains('dark')){
         btn.innerText = 'dark theme';
-        nav.classList.remove('dark');
-        btn.classList.remove('dark');
-        btn.classList.add('light');
-        body.classList.remove('dark');
-        console.log(2);
     }
 })
 
--- js12/3.1/script.js
+++ js12/3.1/script.js
@@ -4,8 +4,8 @@ const sec = document.querySelector('.sec');
 
 setInterval(() => {
     let newDate = new Date();
-    hour.innerText = newDate.getHours();
-    min.innerText = newDate.getMinutes();
-    sec.innerText = newDate.getSeconds();
+    hour.innerText = newDate.getHours() > 9 ? newDate.getHours() : '0' + newDate.getHours();
+    min.innerText = newDate.getMinutes() > 9 ? newDate.getMinutes() : '0' + newDate.getMinutes();
+    sec.innerText = newDate.getSeconds() > 9 ? newDate.getSeconds() : '0' + newDate.getSeconds();
 }, 1000);
 
